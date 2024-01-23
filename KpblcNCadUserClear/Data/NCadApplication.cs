644585473d818e67f6393e9973702b28bd4ba390
kpblc2000 <kpblc2000@gmail.com>
2024-01-23 23:23:53 +0300

    Добавьте файлы проекта.

---
+++ KpblcNCadUserClear/Data/NCadApplication.cs
@@ -0,0 +1,19 @@
+﻿namespace KpblcNCadUserClear.Data
+{
+    internal class NCadApplication
+    {
+        public NCadApplication(string RegistryHiveName)
+        {
+            RegistryName = RegistryHiveName;
+            string[] splited = RegistryHiveName.Split('\\');
+            Version = splited[splited.Length-1];
+            ApplicationFullName = splited[splited.Length - 2] + " " + Version;
+        }
+        /// <summary> Имя узла в реестре, начиная с Software </summary>
+        public string RegistryName { get; }
+        /// <summary> Полное имя приложения для показа в окне </summary>
+        public string ApplicationFullName { get; }
+        /// <summary> "Версия" приложения. Сделана строкой, т.к. может быть нечто типа "23.1 (backup)", что помешает нормальному преобразованию </summary>
+        public string Version { get; }
+    }
+}
