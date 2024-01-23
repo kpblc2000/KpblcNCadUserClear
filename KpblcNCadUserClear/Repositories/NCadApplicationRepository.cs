01c5cd9f96b22f397c08d5c8f1033eadc37ffb68
kpblc2000 <kpblc2000@gmail.com>
2024-01-23 23:28:06 +0300

    Фейковые данные

---
+++ KpblcNCadUserClear/Repositories/NCadApplicationRepository.cs
@@ -0,0 +1,19 @@
+﻿using KpblcNCadUserClear.Data;
+using System.Collections.Generic;
+
+namespace KpblcNCadUserClear.Repositories
+{
+    internal class NCadApplicationRepository
+    {
+        public IEnumerable<NCadApplication> Get()
+        {
+            return new List<NCadApplication>
+            {
+                new NCadApplication(@"Компьютер\HKEY_CURRENT_USER\SOFTWARE\Nanosoft\nanoCAD x64\23.1"),
+                new NCadApplication(@"Компьютер\HKEY_CURRENT_USER\SOFTWARE\Nanosoft\nanoCAD x64\23.0"),
+                new NCadApplication(@"Компьютер\HKEY_CURRENT_USER\SOFTWARE\Nanosoft\nanoCAD x64\24.0"),
+                new NCadApplication(@"Компьютер\HKEY_CURRENT_USER\SOFTWARE\Nanosoft\nanoCAD x64 Plus\20")
+            };
+        }
+    }
+}
