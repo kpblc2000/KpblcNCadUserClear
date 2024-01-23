dbcb3485a1d67f70d7ca686be532348f38c83d57
kpblc2000 <kpblc2000@gmail.com>
2024-01-23 23:31:09 +0300

    Базовая ViewModel

---
+++ KpblcNCadUserClear/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace KpblcNCadUserClear.ViewModels.Base
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+
+        public virtual bool Set<T>(ref T Field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(Field, value))
+            {
+                return false;
+            }
+            Field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        public virtual string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        private string _title;
+    }
+
+}
