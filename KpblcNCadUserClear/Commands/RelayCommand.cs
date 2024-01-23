7ad0c488f95f45b9859c048d8b5916b0a8e50aa1
kpblc2000 <kpblc2000@gmail.com>
2024-01-24 00:43:05 +0300

    Пустая команда для выполнения очистки

---
+++ KpblcNCadUserClear/Commands/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using KpblcNCadUserClear.Commands.Base;
+using System;
+
+namespace KpblcNCadUserClear.Commands
+{
+    internal class RelayCommand : Command
+    {
+
+        private Action<object> _Execute;
+        private Func<object, bool> _CanExecute;
+
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return _CanExecute?.Invoke(parameter) ?? true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _Execute(parameter);
+        }
+    }
+}
--- KpblcNCadUserClear/ViewModels/MainWindowViewModel.cs
+++ KpblcNCadUserClear/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,8 @@ using KpblcNCadUserClear.Repositories;
 using KpblcNCadUserClear.ViewModels.Base;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
+using KpblcNCadUserClear.Commands;
 
 namespace KpblcNCadUserClear.ViewModels
 {
@@ -14,12 +16,26 @@ namespace KpblcNCadUserClear.ViewModels
 
             NCadApplicationRepository rep = new NCadApplicationRepository();
             ApplicationList = new List<NCadApplication>(rep.Get().OrderBy(o => o.Version));
+
+            ClearCommand = new RelayCommand(OnClearCommandExecuted, CanClearCommandExecute);
         }
 
         public List<NCadApplication> ApplicationList
         {
             get => _applicationList;
             private set => Set(ref _applicationList, value);
+        }
+
+        public ICommand ClearCommand { get; }
+
+        public void OnClearCommandExecuted(object p)
+        {
+
+        }
+
+        public bool CanClearCommandExecute(object p)
+        {
+            return ApplicationList.Select(o => o.CheckedToClear).Any();
         }
 
         private List<NCadApplication> _applicationList;
